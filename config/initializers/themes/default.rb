21123ef6c2f5270599fdcc28d2f1d6ad009e0c61
Connor Meth <connormeth27@gmail.com>
2020-01-17 17:56:32 -0500

    integrated spinacms

---
+++ config/initializers/themes/default.rb
@@ -0,0 +1,44 @@
+::Spina::Theme.register do |theme|
+
+  theme.name = 'default'
+  theme.title = 'Default Theme'
+
+  theme.page_parts = [{
+    name:           'content',
+    title:          'Content',
+    partable_type:  'Spina::Image'
+  }, {
+    name:           'text',
+    title:          'Text',
+    partable_type:  'Spina::Text'
+  }]
+
+  theme.view_templates = [{
+    name:       'homepage',
+    title:      'Homepage',
+    page_parts: ['text']
+  }, {
+    name:         'show',
+    title:        'Default',
+    description:  'A standard post theme',
+    usage:        'Use for your content',
+    page_parts:   ['text', 'content']
+  }]
+
+  theme.custom_pages = [{
+    name:           'homepage',
+    title:          'Homepage',
+    deletable:      false,
+    view_template:  'homepage'
+  }]
+
+  theme.navigations = [{
+    name: 'mobile',
+    label: 'Mobile'  
+  }, {
+    name: 'main',
+    label: 'Main navigation',
+    auto_add_pages: false
+  }]
+
+end
